10016e80df2acb6bdbcf45fab2fef07e69f3aefc
Elena081081 <105888936+Elena081081@users.noreply.github.com>
2022-08-09 02:32:58 +0300

    Add files via upload
    Добавлен файл с кодом решения задачи на языке C#

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿string[] array1 = { "12345", "variant", "12", "class", "row", "world", ":)"};
+string[] array2 = new string[array1.Length];
+void FillArray(string[] array1, string[] array2)
+{
+    int count = 0;
+    for (int i = 0; i < array1.Length; i++)
+    {
+        if (array1[i].Length <= 3)
+        {
+            array2[count] = array1[i];
+            count++;
+        }
+    }
+}
+void PrintArray(string[] array2)
+{
+    Console.Write("[");
+    int count = 0;
+    for (int i = 0; i < array1.Length; i++)
+    {
+        if (array1[i].Length <= 3)
+        {
+            array2[count] = array1[i];
+            i++;
+            Console.Write(array2[count] + ", ");
+        }
+    }
+    Console.Write(array2[array1.Length - 1] + "]");
+    Console.WriteLine();
+}
+FillArray(array1, array2);
+PrintArray(array2);
